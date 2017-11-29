b87f8449a846b127699ad6e57d3386ebf3ecbb05
shaack <shaack@gmail.com>
2017-11-29 16:19:23 +0100

    created ModelMock

---
+++ test/mocks/ModelMock.js
@@ -0,0 +1,17 @@
+/**
+ * Author: shaack
+ * Date: 29.11.2017
+ */
+
+export class ModelMock {
+
+    constructor() {
+        this.property1 = 10;
+        this.property2 = 10;
+    }
+
+    add(param1, param2) {
+        return param1 + param2;
+    }
+
+}
