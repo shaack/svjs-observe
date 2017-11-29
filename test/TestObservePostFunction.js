2264cafd03bf996067115e5644bce18acec75958
shaack <shaack@gmail.com>
2017-11-29 18:36:17 +0100

    TestObserve splittet in TestObservePreFunction and TestObservePostFunction

--- test/TestObservePostFunction.js
+++ test/TestObservePostFunction.js
@@ -7,74 +7,7 @@ import {Test} from "../node_modules/svjs-test/src/svjs/Test.js"
 import {Observe} from "../src/svjs/Observe.js";
 import {ModelMock} from "./mocks/ModelMock.js";
 
-export class TestObserve extends Test {
-
-    testPreFunctionSync() {
-        const mock = new ModelMock();
-        let callbackCalled = false;
-        const result1 = mock.add(3, 4);
-        Test.assert(!callbackCalled);
-        Test.assertEquals(7, result1);
-
-        // test callback
-        let observer = Observe.preFunction(mock, "add", (params) => {
-            callbackCalled = true;
-            Test.assertEquals("add", params.functionName);
-            Test.assertEquals(4, params.arguments[1]);
-        }, false); // mode synced
-        const result2 = mock.add(1, 4);
-        Test.assertEquals(5, result2);
-        Test.assert(callbackCalled);
-
-        // remove callback
-        observer.remove();
-        callbackCalled = false;
-        const result3 = mock.add(4, 7);
-        Test.assert(!callbackCalled);
-        Test.assertEquals(11, result3);
-    }
-
-    testPreFunctionSyncModifyArgument() {
-        const mock = new ModelMock();
-        Observe.preFunction(mock, "add", (params) => {
-            Test.assertEquals("add", params.functionName);
-            Test.assertEquals(4, params.arguments[1]);
-            params.arguments[1] = 2;
-        }, false); // mode synced
-        const result = mock.add(1, 4);
-        Test.assertEquals(3, result);
-    }
-
-    testPreFunctionAsync() {
-        const mock = new ModelMock();
-        let callbackCalled = false;
-        let callbackCallCount = 0;
-        const result1 = mock.add(3, 4);
-        Test.assert(!callbackCalled);
-        Test.assertEquals(7, result1);
-
-        // test callback
-        let observer = Observe.preFunction(mock, "add", (params) => {
-            callbackCalled = true;
-            callbackCallCount++;
-            Test.assertEquals("add", params.functionName);
-            Test.assertEquals(4, params.arguments[1]);
-        }); // mode async
-        const result2 = mock.add(1, 4);
-        Test.assertEquals(5, result2);
-        Test.assert(!callbackCalled);
-        setTimeout(() => { // wait one tick for async
-            Test.assert(callbackCalled); // you can see this only in console
-            Test.assertEquals(1, callbackCallCount);
-        });
-
-        // remove callback
-        observer.remove();
-        callbackCalled = false;
-        const result3 = mock.add(4, 7);
-        Test.assert(!callbackCalled);
-        Test.assertEquals(11, result3);
-    }
+export class TestObservePostFunction extends Test {
 
     testPostFunctionSync() {
         const mock = new ModelMock();
@@ -108,6 +41,38 @@ export class TestObserve extends Test {
             params.returnValue = 42;
         }, false); // mode synced
         Test.assertEquals(42, mock.add(1, 2));
+    }
+
+    testPostFunctionAsync() {
+        const mock = new ModelMock();
+        let callbackCalled = false;
+        let callbackCallCount = 0;
+        const result1 = mock.add(3, 4);
+        Test.assert(!callbackCalled);
+        Test.assertEquals(7, result1);
+
+        // test callback
+        let observer = Observe.postFunction(mock, "add", (params) => {
+            callbackCalled = true;
+            callbackCallCount++;
+            Test.assertEquals("add", params.functionName);
+            Test.assertEquals(4, params.arguments[1]);
+            Test.assertEquals(5, params.returnValue);
+        }); // mode async
+        const result2 = mock.add(1, 4);
+        Test.assertEquals(5, result2);
+        Test.assert(!callbackCalled);
+        setTimeout(() => { // wait one tick for async
+            Test.assert(callbackCalled); // you can see this only in console
+            Test.assertEquals(1, callbackCallCount);
+        });
+
+        // remove callback
+        observer.remove();
+        callbackCalled = false;
+        const result3 = mock.add(4, 7);
+        Test.assert(!callbackCalled);
+        Test.assertEquals(11, result3);
     }
 
 }
